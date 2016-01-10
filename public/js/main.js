3c42be7988b5c7754c2f34befe7e065f6e5a771e
Vladislav Dekov <vdekoff@gmail.com>
2016-01-10 22:53:17 +0200

    Close WebSocket connection on window unload. Use charCode in Firefox.

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,23 @@
 (function () {
    
-   function EchoConnection ( ws_location ) {
+   function EchoConnection () {
       /*------------- DOM Elements references --------------------------------*/
       this.input  = document.getElementsByName( 'input' )[0];
       this.output = document.getElementsByName( 'output' )[0];
       this.status = document.querySelector( 'div.status' );
       
-      this.ws_location = ws_location;               // WebSocket server address
+      this.ws_location = 'ws://127.0.0.1:1337';     // WebSocket server address
       this.connection  = null;                      // WebSocket.Client instance
       this.interval    = null;                      // The value of setInterval
       this.period      = 3000;                      // The time on which the client would try to connect
                                                     // to the server when the connection is down
       
       this.callbacks   = {
-         sendToServer : this.sendToServer.bind( this ),
-         onOpen       : this.onOpen.bind( this ),
-         onMessage    : this.onMessage.bind( this ),
-         onClose      : this.onClose.bind( this )
+         onOpen          : this.onOpen.bind( this ),
+         onMessage       : this.onMessage.bind( this ),
+         onClose         : this.onClose.bind( this ),
+         sendToServer    : this.sendToServer.bind( this ),
+         closeConnection : this.closeConnection.bind( this )
       };
    };
    
@@ -26,17 +27,25 @@
          this.connection = new WebSocket( this.ws_location );
          clearInterval( this.interval );
          
+         this.initCallbacks();
+      },
+      
+      initCallbacks : function () {
          // Add WebSocket event listeners
          this.connection.addEventListener( 'open', this.callbacks.onOpen );
          this.connection.addEventListener( 'message', this.callbacks.onMessage );
          this.connection.addEventListener( 'close', this.callbacks.onClose );
+         
+         // Close the WebSocket connection on window unload because Firefox
+         // https://bugzilla.mozilla.org/show_bug.cgi?id=712329
+         window.addEventListener( 'beforeunload', this.callbacks.closeConnection );
       },
       
       // Executes when we are connected successfully to the server
       onOpen : function () {
          this.resetFields();
          
-         this.status.innerText = 'Connected';
+         this.status.innerHTML = 'Connected';
          this.status.classList.remove( 'disconnected' );
          this.status.classList.add( 'connected' );
          this.input.addEventListener( 'keypress', this.callbacks.sendToServer );
@@ -57,7 +66,7 @@
          }
          
          // Set the message as output field value
-         // Conver the value `toString` because Safari
+         // Parse the value `toString` because Safari
          this.output.value = message.string.toString();
          
          // Check for a received command
@@ -68,7 +77,7 @@
       
       // Handles 'close' event of the WebSocket
       onClose : function ( connection ) {
-         this.status.innerText = 'Disconnected';
+         this.status.innerHTML = 'Disconnected';
          this.status.classList.remove( 'connected' );
          this.status.classList.add( 'disconnected' );
          this.input.removeEventListener( 'keypress', this.callbacks.sendToServer );
@@ -82,8 +91,11 @@
       
       // Send message to the WebSocket server
       sendToServer : function ( event ) {
+         // Use `charCode` property to get the character code (because Firefox)
+         var code = event.charCode || event.keyCode;
+         
          // Send to the WebSocket server the entered character
-         this.connection.send( String.fromCharCode( event.keyCode ) );
+         this.connection.send( String.fromCharCode( code ) );
          this.input.value = '';
       },
       
@@ -91,8 +103,12 @@
       resetFields : function () {
          this.input.value = '';
          this.output.value = '';
+      },
+      
+      closeConnection : function () {
+         this.connection.close();
       }
    };
    
-   window.EchoConnection = new EchoConnection( 'ws://127.0.0.1:1337' );
+   window.EchoConnection = new EchoConnection();
 })();
